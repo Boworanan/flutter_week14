b68f5ed76a70d1213692b876a9b1f13eef1f3fe5
Boworanan <bowonranan.30@gmail.com>
2022-08-18 15:41:11 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'contact.dart';
+import 'home.dart';
 
 class MyApp extends StatelessWidget{
   const MyApp({Key? key}):super(key: key);
--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-class ContactPage extends StatelessWidget {
-  const ContactPage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Contact"),
+        title: Text("MyApp"),
       ),
       body: Center(
-        child: Text("Contact App"),
+        child: Text("Home App"),
       ),
     );
   }
