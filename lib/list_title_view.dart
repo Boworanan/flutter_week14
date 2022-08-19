301ec84a1a18ccbee2aba072b48bf165a5453c48
Boworanan <bowonranan.30@gmail.com>
2022-08-19 16:01:15 +0700

    update

--- lib/list_title_view.dart
+++ lib/list_title_view.dart
@@ -7,10 +7,35 @@ class ListTitlePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Flutter ListTitle"),backgroundColor: Colors.purpleAccent,),
-      body:ListTile(
-        title: Text("WAVE",style: TextStyle(fontSize: 15),),
-        subtitle: Text("I was born on Earth"" but I heard me say u are my heart."),
-        leading: Icon(Icons.account_circle_sharp,color: Colors.greenAccent,size: 50,),
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("WAVE",style: TextStyle(fontSize: 15),),
+              subtitle: Text("I was born on Earth"" but I heard me say u are my heart."),
+              leading: Icon(Icons.account_circle_sharp,color: Colors.greenAccent,size: 50,),
+              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete,size: 30,)),
+            ),
+            ListTile(
+              title: Text("NAN",style: TextStyle(fontSize: 15),),
+              subtitle: Text("I love every children"),
+              leading: Icon(Icons.account_circle_sharp,color: Colors.greenAccent,size: 50,),
+              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete,size: 30,)),
+            ),
+            ListTile(
+              title: Text("BEEM",style: TextStyle(fontSize: 15),),
+              subtitle: Text("I am FCBoxing teen"),
+              leading: Icon(Icons.account_circle_sharp,color: Colors.greenAccent,size: 50,),
+              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete,size: 30,)),
+            ),
+            ListTile(
+              title: Text("GATE",style: TextStyle(fontSize: 15),),
+              subtitle: Text("I want to dance every time when i hear Kpop music"),
+              leading: Icon(Icons.account_circle_sharp,color: Colors.greenAccent,size: 50,),
+              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete,size: 30,)),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.purpleAccent,
